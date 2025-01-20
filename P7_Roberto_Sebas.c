2c968e7663141f2cda1d8a466785652f5f4206c4
Sebas Silva Narro <sebassilvanarro@gmail.com>
2025-01-20 09:22:34 +0100

    añadiendo el gitignore

--- P7_Roberto_Sebas.c
+++ P7_Roberto_Sebas.c
@@ -1,4 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Autores: Roberto y Sebas
 
+Num * añadirNumero(int input, int num);
+
+int main(){
+	int input;
+
+	printf("¿Cuántos números quieres ordenar? ");
+	scanf("%d", &input);
+
+
+
+	return 0;
+}
+
+int arrNums = (int *)malloc(input * sizeof(int));
+
+if (arrNums == NULL){
+	printf("Error.\n");
+	return 1;
+}
+
+Num * añadirNumero(int input, int num){
+
+}
